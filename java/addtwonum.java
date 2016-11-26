7bc087786eb1d97eef380286b8a246f89c186b64
Teju Shete <tejushete26@gmail.com>
2016-11-26 09:58:04 +0530

    First Code

---
+++ java/addtwonum.java
@@ -0,0 +1,16 @@
+class addtwonum{
+
+   public static int add(int num1,int num2){
+   	int sum=num1+num2;
+   	return sum;
+ }
+
+  public static void main(String[] java){
+
+   int result = add(40,50);
+
+   System.out.println(result);
+
+  }
+
+}
